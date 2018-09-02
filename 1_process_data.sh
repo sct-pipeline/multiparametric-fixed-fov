ce28f092853866fdaaff09313ea20a02fe47e58a
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-09-02 12:26:14 -0400

    Updated parameters_template and added QC

--- 1_process_data.sh
+++ 1_process_data.sh
@@ -44,12 +44,12 @@ if [ ! -z ${MIDFOV_DISC} ]; then
   # create disc label in the middle of the S-I direction at the center of the cord
   sct_label_utils -i mt1_crop.nii.gz -create-seg -1,$MIDFOV_DISC -o label_disc.nii.gz
   # Register template->mt1
-  sct_register_to_template -i mt1_crop.nii.gz -s ${file_seg} -ldisc label_disc.nii.gz -ref subject -c t2 -param step=1,type=seg,algo=centermass:step=2,type=im,algo=syn,metric=CC,slicewise=0,smooth=0,iter=3 -qc ../../../qc
+  sct_register_to_template -i mt1_crop.nii.gz -s ${file_seg} -ldisc label_disc.nii.gz -ref subject -c t2 -param step=1,type=seg,algo=centermass:step=2,type=im,algo=syn,metric=CC,slicewise=0,smooth=0,iter=3 -qc ${PATH_QC}
 elif [ ! -z ${MIDFOV_VERT} ]; then
   # create vert label in the middle of the S-I direction at the center of the cord
   sct_label_utils -i mt1_crop.nii.gz -create-seg -1,$MIDFOV_VERT -o label_vert.nii.gz
-  # Register template->mt1
-  sct_register_to_template -i mt1_crop.nii.gz -s ${file_seg} -l label_vert.nii.gz -ref subject -c t2 -param step=1,type=seg,algo=centermass:step=2,type=im,algo=syn,metric=CC,slicewise=0,smooth=0,iter=3 -qc ../../../qc
+  # Register template->t1w
+  sct_register_to_template -i mt1_crop.nii.gz -s ${file_seg} -l label_vert.nii.gz -ref subject -c t2 -param step=1,type=seg,algo=centermass:step=2,type=im,algo=syn,metric=CC,slicewise=0,smooth=0,iter=3 -qc ${PATH_QC}
 else
   printf "${Red}${On_Black}\nERROR: Neither MIDFOV_DISC nor MIDFOV_VERT field is active. Please see README.md. Exiting.\n\n${Color_Off}"
   exit 1
--- parameters_template.sh
+++ parameters_template.sh
@@ -3,25 +3,33 @@
 # Copy this file and rename it as `parameters.sh`, then modify the variables
 # according to your needs.
 
-# Path to input data (do not add "/" at the end). This path should be absolute
-# (i.e. should start with "/"). Example: PATH_DATA="/Users/bob/data"
-export PATH_DATA=""
+# Path to working directory, which contains data results, qc, etc. (do not add "/" at the end). 
+# This path should be absolute (i.e. should start with "/"). Example: PATH_DATA="/Users/bob/experiments"
+export PATH_MAIN="/Users/julien/data/rainville_lumbar"
+
+# Path to the folder which contains all data
+export PATH_DATA="${PATH_MAIN}/data"
 
 # List of subjects to analyse. If you want to analyze all subjects in the
 # PATH_DATA folder, then comment this variable.
 export SUBJECTS=(
-	"001"
-	"002"
+	"slbp_001p"
+	"SLBP_002"
+	"slbp_004"
+	"slbp_005"
+	"slbp_006"
 	)
 
-# Vertebral or disc level where FOV is centered. Uncomment the appropriate
+# vertebral or disc level where FOV is centered. Uncomment the appropriate
 # variable and assign value:
 # export MIDFOV_DISC=X
-# export MIDFOV_VERT=
+# export MIDFOV_VERT=X
 
-# Vertebral levels to compute MRI metrics from. Please replace the example
-# values below.
+# Vertebral levels to compute MRI metrics from
 export METRICS_VERT_LEVEL="3,4,5"
 
 # Path where to save results (do not add "/" at the end).
-export PATH_RESULTS="${PATH_DATA}/results"
+export PATH_RESULTS="${PATH_MAIN}/results"
+
+# Path where to save QC results (do not add "/" at the end).
+export PATH_QC="${PATH_MAIN}/qc"
